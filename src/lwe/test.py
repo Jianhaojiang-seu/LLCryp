fa2eb60c36c7abe9d3ca859e5c0100b18ef54c38
Mayank Dhiman <mdhiman536@gmail.com>
2020-06-26 23:14:27 +0530

    Added .gitignore

---
+++ src/lwe/test.py
@@ -0,0 +1,9 @@
+# from fetchKeys import fetchPublicKeys
+# import math
+# import random
+
+# print(transformed_location)
+# geolockMapping = self.createGeoLock(longitude, latitude, tolerance)
+# sessionKey = self.getSessionKey()
+# data = self.getXOR(geolockMapping, sessionKey)
+# cipher_text = self.LWE_encryption(data)
