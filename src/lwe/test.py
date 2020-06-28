32b322fd04565296f3a3479b4141d72e0c98535b
yash17525 <yashwant2451@gmail.com>
2020-06-28 22:10:43 +0530

    tested LWE

--- src/lwe/test.py
+++ src/lwe/test.py
@@ -7,3 +7,21 @@
 # sessionKey = self.getSessionKey()
 # data = self.getXOR(geolockMapping, sessionKey)
 # cipher_text = self.LWE_encryption(data)
+
+
+from LWE import Lwe
+
+L = Lwe()
+data = '122'
+secrectKey = 20000.0234352
+encryptedData = L.LWE_encryption(data, secrectKey)
+print(encryptedData)
+
+decryptedData = L.LWE_decryption(encryptedData, secrectKey, 97)
+print(decryptedData)
+
+# data = 'my name is yash`'
+
+# s = data.encode('utf-8')
+# print(s)
+# print( s.decode('utf-8') )
